430107563192d6ca38dd4aa90b2d6cdfce555335
fishington <fishera34@mymacewan.ca>
2025-03-26 18:57:03 -0600

    Init for new Proj

---
+++ ABF-Portfolio/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to Home Page</h1>
+        <p>This is the homepage of our Vite + React app.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
